ec768ace9cbc53cba00e60414da2d18fbe2170a2
Arshad <ritchiearshu@gmail.com>
2025-06-23 22:12:42 +0530

    "Day 1 project setup"

---
+++ Banking-Application/src/main/java/com/example/bank/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.bank.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@GetMapping("/hello")
+	public String getHello()
+	{
+		return "Hello, Arsh! Welcome to your Banking App 🚀";
+	}
+
+}
